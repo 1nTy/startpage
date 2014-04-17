5cd8a304fc7809330d94dd2b76bd40ee78881c1a
1nTy <federico.fini.1992@gmail.com>
2014-04-17 08:50:35 +0200

    Update
    Under the hood improvement

---
+++ scripts/icons.js
@@ -0,0 +1,114 @@
+/*	SOCIAL	*/
+
+	loadImage1 = new Image();
+	loadImage1.src = "img/fb_t.png";
+	staticImage1 = new Image();
+	staticImage1.src = "img/fb_n.png";
+
+	loadImage2 = new Image();
+	loadImage2.src = "img/twitter_t.svg";
+	staticImage2 = new Image();
+	staticImage2.src = "img/twitter_n.svg";
+
+	loadImage3 = new Image();
+	loadImage3.src = "img/googleplus_t.png";
+	staticImage3 = new Image();
+	staticImage3.src = "img/googleplus_n.png";
+
+	loadImage4 = new Image();
+	loadImage4.src = "img/tumblr_t.png";
+	staticImage4 = new Image();
+	staticImage4.src = "img/tumblr_n.png";
+
+	loadImage5 = new Image();
+	loadImage5.src = "img/youtube_t.png";
+	staticImage5 = new Image();
+	staticImage5.src = "img/youtube_n.png";
+
+
+/*	CLOUD	*/
+
+	loadImage8 = new Image();
+	loadImage8.src = "img/dropbox_t.svg";
+	staticImage8 = new Image();
+	staticImage8.src = "img/dropbox_n.svg";
+
+	loadImage9 = new Image();
+	loadImage9.src = "img/box_t.png";
+	staticImage9 = new Image();
+	staticImage9.src = "img/box_n.png";
+
+	loadImage10 = new Image();
+	loadImage10.src = "img/onedrive_t.png";
+	staticImage10 = new Image();
+	staticImage10.src = "img/onedrive_n.png";
+
+	loadImage11 = new Image();
+	loadImage11.src = "img/googledrive_t.png";
+	staticImage11 = new Image();
+	staticImage11.src = "img/googledrive_n.png";
+
+	loadImage12 = new Image();
+	loadImage12.src = "img/mega_t.png";
+	staticImage12 = new Image();
+	staticImage12.src = "img/mega_n.png";
+	
+
+/*	GRAPHIC	*/
+
+	loadImage6 = new Image();
+	loadImage6.src = "img/deviantart_t.png";
+	staticImage6 = new Image();
+	staticImage6.src = "img/deviantart_n.png";
+
+	loadImage7 = new Image();
+	loadImage7.src = "img/kuler_t.png";
+	staticImage7 = new Image();
+	staticImage7.src = "img/kuler_n.png";
+
+
+/*	NEWS	*/
+
+	loadImage13 = new Image();
+	loadImage13.src = "img/feedly_t.png";
+	staticImage13 = new Image();
+	staticImage13.src = "img/feedly_n.png";
+
+	loadImage18 = new Image();
+	loadImage18.src = "img/pocket_t.png";
+	staticImage18 = new Image();
+	staticImage18.src = "img/pocket_n.png";
+
+
+/*	STORE */
+
+	loadImage14 = new Image();
+	loadImage14.src = "img/amazon_t.png";
+	staticImage14 = new Image();
+	staticImage14.src = "img/amazon_n.png";
+
+	loadImage15 = new Image();
+	loadImage15.src = "img/ebay_t.png";
+	staticImage15 = new Image();
+	staticImage15.src = "img/ebay_n.png";
+
+	loadImage16 = new Image();
+	loadImage16.src = "img/steam_t.png";
+	staticImage16 = new Image();
+	staticImage16.src = "img/steam_n.png";
+
+
+/*	FORUM	*/
+
+	loadImage17 = new Image();
+	loadImage17.src = "img/arch_t.png";
+	staticImage17 = new Image();
+	staticImage17.src = "img/arch_n.png";
+
+	
+/*	FUN	*/
+
+	loadImage19 = new Image();
+	loadImage19.src = "img/ninegag_t.png";
+	staticImage19 = new Image();
+	staticImage19.src = "img/ninegag_n.png";
