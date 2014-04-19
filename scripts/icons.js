f84682cb43a76d1cc775c27197fd9666a2ce9d9b
1nTy <federico.fini.1992@gmail.com>
2014-04-19 14:47:30 +0200

    Misc update
    Added new icons
    Added new links
    Icon script updated

--- scripts/icons.js
+++ scripts/icons.js
@@ -1,114 +1,134 @@
 /*	SOCIAL	*/
 
-	loadImage1 = new Image();
-	loadImage1.src = "img/fb_t.png";
-	staticImage1 = new Image();
-	staticImage1.src = "img/fb_n.png";
+	load_fb = new Image();
+	load_fb.src = "img/fb_t.png";
+	norm_fb = new Image();
+	norm_fb.src = "img/fb_n.png";
 
-	loadImage2 = new Image();
-	loadImage2.src = "img/twitter_t.svg";
-	staticImage2 = new Image();
-	staticImage2.src = "img/twitter_n.svg";
+	load_twitt = new Image();
+	load_twitt.src = "img/twitter_t.svg";
+	norm_twitt = new Image();
+	norm_twitt.src = "img/twitter_n.svg";
 
-	loadImage3 = new Image();
-	loadImage3.src = "img/googleplus_t.png";
-	staticImage3 = new Image();
-	staticImage3.src = "img/googleplus_n.png";
+	load_gplus = new Image();
+	load_gplus.src = "img/googleplus_t.png";
+	norm_gplus = new Image();
+	norm_gplus.src = "img/googleplus_n.png";
 
-	loadImage4 = new Image();
-	loadImage4.src = "img/tumblr_t.png";
-	staticImage4 = new Image();
-	staticImage4.src = "img/tumblr_n.png";
-
-	loadImage5 = new Image();
-	loadImage5.src = "img/youtube_t.png";
-	staticImage5 = new Image();
-	staticImage5.src = "img/youtube_n.png";
+	load_tumblr = new Image();
+	load_tumblr.src = "img/tumblr_t.png";
+	norm_tumblr = new Image();
+	norm_tumblr.src = "img/tumblr_n.png";
 
 
 /*	CLOUD	*/
 
-	loadImage8 = new Image();
-	loadImage8.src = "img/dropbox_t.svg";
-	staticImage8 = new Image();
-	staticImage8.src = "img/dropbox_n.svg";
+	load_db = new Image();
+	load_db.src = "img/dropbox_t.svg";
+	norm_db = new Image();
+	norm_db.src = "img/dropbox_n.svg";
 
-	loadImage9 = new Image();
-	loadImage9.src = "img/box_t.png";
-	staticImage9 = new Image();
-	staticImage9.src = "img/box_n.png";
+	load_box = new Image();
+	load_box.src = "img/box_t.png";
+	norm_box = new Image();
+	norm_box.src = "img/box_n.png";
 
-	loadImage10 = new Image();
-	loadImage10.src = "img/onedrive_t.png";
-	staticImage10 = new Image();
-	staticImage10.src = "img/onedrive_n.png";
+	load_1drive = new Image();
+	load_1drive.src = "img/onedrive_t.png";
+	norm_1drive = new Image();
+	norm_1drive.src = "img/onedrive_n.png";
 
-	loadImage11 = new Image();
-	loadImage11.src = "img/googledrive_t.png";
-	staticImage11 = new Image();
-	staticImage11.src = "img/googledrive_n.png";
+	load_gdrive = new Image();
+	load_gdrive.src = "img/googledrive_t.png";
+	norm_gdrive = new Image();
+	norm_gdrive.src = "img/googledrive_n.png";
 
-	loadImage12 = new Image();
-	loadImage12.src = "img/mega_t.png";
-	staticImage12 = new Image();
-	staticImage12.src = "img/mega_n.png";
-	
+	load_mega = new Image();
+	load_mega.src = "img/mega_t.png";
+	norm_mega = new Image();
+	norm_mega.src = "img/mega_n.png";
+
 
 /*	GRAPHIC	*/
 
-	loadImage6 = new Image();
-	loadImage6.src = "img/deviantart_t.png";
-	staticImage6 = new Image();
-	staticImage6.src = "img/deviantart_n.png";
+	load_da = new Image();
+	load_da.src = "img/deviantart_t.png";
+	norm_da = new Image();
+	norm_da.src = "img/deviantart_n.png";
 
-	loadImage7 = new Image();
-	loadImage7.src = "img/kuler_t.png";
-	staticImage7 = new Image();
-	staticImage7.src = "img/kuler_n.png";
+	load_kuler = new Image();
+	load_kuler.src = "img/kuler_t.png";
+	norm_kuler = new Image();
+	norm_kuler.src = "img/kuler_n.png";
+
+
+/*	MULTIMEDIA	*/
+
+	load_yt = new Image();
+	load_yt.src = "img/youtube_t.png";
+	norm_yt = new Image();
+	norm_yt.src = "img/youtube_n.png";
+
+	load_sd = new Image();
+	load_sd.src = "img/soundcloud_t.png";
+	norm_sd = new Image();
+	norm_sd.src = "img/soundcloud_n.png";
 
 
 /*	NEWS	*/
 
-	loadImage13 = new Image();
-	loadImage13.src = "img/feedly_t.png";
-	staticImage13 = new Image();
-	staticImage13.src = "img/feedly_n.png";
+	load_feedly = new Image();
+	load_feedly.src = "img/feedly_t.png";
+	norm_feedly = new Image();
+	norm_feedly.src = "img/feedly_n.png";
 
-	loadImage18 = new Image();
-	loadImage18.src = "img/pocket_t.png";
-	staticImage18 = new Image();
-	staticImage18.src = "img/pocket_n.png";
+	load_pocket = new Image();
+	load_pocket.src = "img/pocket_t.png";
+	norm_pocket = new Image();
+	norm_pocket.src = "img/pocket_n.png";
+
+	load_evernote = new Image();
+	load_evernote.src = "img/evernote_t.png";
+	norm_evernote = new Image();
+	norm_evernote.src = "img/evernote_n.png";
 
 
 /*	STORE */
 
-	loadImage14 = new Image();
-	loadImage14.src = "img/amazon_t.png";
-	staticImage14 = new Image();
-	staticImage14.src = "img/amazon_n.png";
+	load_amazon = new Image();
+	load_amazon.src = "img/amazon_t.png";
+	norm_amazon = new Image();
+	norm_amazon.src = "img/amazon_n.png";
 
-	loadImage15 = new Image();
-	loadImage15.src = "img/ebay_t.png";
-	staticImage15 = new Image();
-	staticImage15.src = "img/ebay_n.png";
+	load_ebay = new Image();
+	load_ebay.src = "img/ebay_t.png";
+	norm_ebay = new Image();
+	norm_ebay.src = "img/ebay_n.png";
 
-	loadImage16 = new Image();
-	loadImage16.src = "img/steam_t.png";
-	staticImage16 = new Image();
-	staticImage16.src = "img/steam_n.png";
+	load_steam = new Image();
+	load_steam.src = "img/steam_t.png";
+	norm_steam = new Image();
+	norm_steam.src = "img/steam_n.png";
 
 
 /*	FORUM	*/
 
-	loadImage17 = new Image();
-	loadImage17.src = "img/arch_t.png";
-	staticImage17 = new Image();
-	staticImage17.src = "img/arch_n.png";
+	load_arch = new Image();
+	load_arch.src = "img/arch_t.png";
+	norm_arch = new Image();
+	norm_arch.src = "img/arch_n.png";
 
 	
 /*	FUN	*/
 
-	loadImage19 = new Image();
-	loadImage19.src = "img/ninegag_t.png";
-	staticImage19 = new Image();
-	staticImage19.src = "img/ninegag_n.png";
+	load_9gag = new Image();
+	load_9gag.src = "img/ninegag_t.png";
+	norm_9gag = new Image();
+	norm_9gag.src = "img/ninegag_n.png";
+
+/*	DEVELOPER	*/
+
+	load_github = new Image();
+	load_github.src = "img/github_t.png";
+	norm_github = new Image();
+	norm_github.src = "img/github_n.png";
---
+++ scripts/settings.js
@@ -0,0 +1,7 @@
+$(document).ready(function() 
+{
+	
+	/*	Use "true" to enable feature, "false" to disable feature	*/
+	
+		newwindow = false;
+});
