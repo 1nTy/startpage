ba740854cd1f0a93ce6362112edbfdf7582de128
1nTy <federico.fini.1992@gmail.com>
2014-04-21 15:28:00 +0200

    Misc update #2
    Added google link
    Under the hood improvement
    Changed style
    New screens

--- scripts/icons.js
+++ scripts/icons.js
@@ -1,134 +1,141 @@
+/*	SEARCH	*/
+
+	load_google = new Image();
+	load_google.src = "images/search/google_t.png";
+	norm_google = new Image();
+	norm_google.src = "images/search/google_n.png";
+
 /*	SOCIAL	*/
 
 	load_fb = new Image();
-	load_fb.src = "img/fb_t.png";
+	load_fb.src = "images/icons/fb_t.png";
 	norm_fb = new Image();
-	norm_fb.src = "img/fb_n.png";
+	norm_fb.src = "images/icons/fb_n.png";
 
 	load_twitt = new Image();
-	load_twitt.src = "img/twitter_t.svg";
+	load_twitt.src = "images/icons/twitter_t.svg";
 	norm_twitt = new Image();
-	norm_twitt.src = "img/twitter_n.svg";
+	norm_twitt.src = "images/icons/twitter_n.svg";
 
 	load_gplus = new Image();
-	load_gplus.src = "img/googleplus_t.png";
+	load_gplus.src = "images/icons/googleplus_t.png";
 	norm_gplus = new Image();
-	norm_gplus.src = "img/googleplus_n.png";
+	norm_gplus.src = "images/icons/googleplus_n.png";
 
 	load_tumblr = new Image();
-	load_tumblr.src = "img/tumblr_t.png";
+	load_tumblr.src = "images/icons/tumblr_t.png";
 	norm_tumblr = new Image();
-	norm_tumblr.src = "img/tumblr_n.png";
+	norm_tumblr.src = "images/icons/tumblr_n.png";
 
 
 /*	CLOUD	*/
 
 	load_db = new Image();
-	load_db.src = "img/dropbox_t.svg";
+	load_db.src = "images/icons/dropbox_t.svg";
 	norm_db = new Image();
-	norm_db.src = "img/dropbox_n.svg";
+	norm_db.src = "images/icons/dropbox_n.svg";
 
 	load_box = new Image();
-	load_box.src = "img/box_t.png";
+	load_box.src = "images/icons/box_t.png";
 	norm_box = new Image();
-	norm_box.src = "img/box_n.png";
+	norm_box.src = "images/icons/box_n.png";
 
 	load_1drive = new Image();
-	load_1drive.src = "img/onedrive_t.png";
+	load_1drive.src = "images/icons/onedrive_t.png";
 	norm_1drive = new Image();
-	norm_1drive.src = "img/onedrive_n.png";
+	norm_1drive.src = "images/icons/onedrive_n.png";
 
 	load_gdrive = new Image();
-	load_gdrive.src = "img/googledrive_t.png";
+	load_gdrive.src = "images/icons/googledrive_t.png";
 	norm_gdrive = new Image();
-	norm_gdrive.src = "img/googledrive_n.png";
+	norm_gdrive.src = "images/icons/googledrive_n.png";
 
 	load_mega = new Image();
-	load_mega.src = "img/mega_t.png";
+	load_mega.src = "images/icons/mega_t.png";
 	norm_mega = new Image();
-	norm_mega.src = "img/mega_n.png";
+	norm_mega.src = "images/icons/mega_n.png";
 
 
 /*	GRAPHIC	*/
 
 	load_da = new Image();
-	load_da.src = "img/deviantart_t.png";
+	load_da.src = "images/icons/deviantart_t.png";
 	norm_da = new Image();
-	norm_da.src = "img/deviantart_n.png";
+	norm_da.src = "images/icons/deviantart_n.png";
 
 	load_kuler = new Image();
-	load_kuler.src = "img/kuler_t.png";
+	load_kuler.src = "images/icons/kuler_t.png";
 	norm_kuler = new Image();
-	norm_kuler.src = "img/kuler_n.png";
+	norm_kuler.src = "images/icons/kuler_n.png";
 
 
 /*	MULTIMEDIA	*/
 
 	load_yt = new Image();
-	load_yt.src = "img/youtube_t.png";
+	load_yt.src = "images/icons/youtube_t.png";
 	norm_yt = new Image();
-	norm_yt.src = "img/youtube_n.png";
+	norm_yt.src = "images/icons/youtube_n.png";
 
 	load_sd = new Image();
-	load_sd.src = "img/soundcloud_t.png";
+	load_sd.src = "images/icons/soundcloud_t.png";
 	norm_sd = new Image();
-	norm_sd.src = "img/soundcloud_n.png";
+	norm_sd.src = "images/icons/soundcloud_n.png";
 
 
 /*	NEWS	*/
 
 	load_feedly = new Image();
-	load_feedly.src = "img/feedly_t.png";
+	load_feedly.src = "images/icons/feedly_t.png";
 	norm_feedly = new Image();
-	norm_feedly.src = "img/feedly_n.png";
+	norm_feedly.src = "images/icons/feedly_n.png";
 
 	load_pocket = new Image();
-	load_pocket.src = "img/pocket_t.png";
+	load_pocket.src = "images/icons/pocket_t.png";
 	norm_pocket = new Image();
-	norm_pocket.src = "img/pocket_n.png";
+	norm_pocket.src = "images/icons/pocket_n.png";
 
 	load_evernote = new Image();
-	load_evernote.src = "img/evernote_t.png";
+	load_evernote.src = "images/icons/evernote_t.png";
 	norm_evernote = new Image();
-	norm_evernote.src = "img/evernote_n.png";
+	norm_evernote.src = "images/icons/evernote_n.png";
 
 
 /*	STORE */
 
 	load_amazon = new Image();
-	load_amazon.src = "img/amazon_t.png";
+	load_amazon.src = "images/icons/amazon_t.png";
 	norm_amazon = new Image();
-	norm_amazon.src = "img/amazon_n.png";
+	norm_amazon.src = "images/icons/amazon_n.png";
 
 	load_ebay = new Image();
-	load_ebay.src = "img/ebay_t.png";
+	load_ebay.src = "images/icons/ebay_t.png";
 	norm_ebay = new Image();
-	norm_ebay.src = "img/ebay_n.png";
+	norm_ebay.src = "images/icons/ebay_n.png";
 
 	load_steam = new Image();
-	load_steam.src = "img/steam_t.png";
+	load_steam.src = "images/icons/steam_t.png";
 	norm_steam = new Image();
-	norm_steam.src = "img/steam_n.png";
+	norm_steam.src = "images/icons/steam_n.png";
 
 
 /*	FORUM	*/
 
 	load_arch = new Image();
-	load_arch.src = "img/arch_t.png";
+	load_arch.src = "images/icons/arch_t.png";
 	norm_arch = new Image();
-	norm_arch.src = "img/arch_n.png";
+	norm_arch.src = "images/icons/arch_n.png";
 
 	
 /*	FUN	*/
 
 	load_9gag = new Image();
-	load_9gag.src = "img/ninegag_t.png";
+	load_9gag.src = "images/icons/ninegag_t.png";
 	norm_9gag = new Image();
-	norm_9gag.src = "img/ninegag_n.png";
+	norm_9gag.src = "images/icons/ninegag_n.png";
 
 /*	DEVELOPER	*/
 
 	load_github = new Image();
-	load_github.src = "img/github_t.png";
+	load_github.src = "images/icons/github_t.png";
 	norm_github = new Image();
-	norm_github.src = "img/github_n.png";
+	norm_github.src = "images/icons/github_n.png";
